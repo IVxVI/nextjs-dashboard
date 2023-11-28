ad40fa046b9c7895d9a6473c50abb3c7957ed4ee
IVxVI <ivan665303@gmail.com>
2023-11-28 21:45:25 +0200

    finished chapter 15 and 16

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
- 
+import { signIn } from '@/auth';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -109,5 +110,19 @@ export async function deleteInvoice(id: string) {
     return {message: 'Successfully Deleted Invoice'}
   } catch (error) {
     return {message: 'Database error: Failed to Delete Invoice'}
+  }
+}
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
